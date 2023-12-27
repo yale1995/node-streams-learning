f83458e8c059bc265d1df22c978723e74da8a984
Yale Araujo <yale850@gmail.com>
2023-12-27 09:26:48 -0300

    docs: comments about http methods

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
 import http from 'node:http'
 
+// Métodos HTTP: combinação de método + url
+// 1. Método
+// 2. URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET --> Buscar um recurso no backend
+// POST --> Criar um recurso no backend
+// PUT --> Atualizar um recurso no backend
+// PATCH --> Atualizar uma informação específica de um recurso no backend
+// DELETE --> Deletar um recurso específico do backend
+
 const server = http.createServer((req, res) => {
     return res.end('Hello World')
 })

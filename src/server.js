a27534e6efcd2dc6a46249650240eeed99a3db84
Yale Araujo <yale850@gmail.com>
2023-12-27 09:30:53 -0300

    feat: get users list and create new user features

--- src/server.js
+++ src/server.js
@@ -13,6 +13,16 @@ import http from 'node:http'
 // DELETE --> Deletar um recurso específico do backend
 
 const server = http.createServer((req, res) => {
+    const {method, url} = req
+    
+    if (method === 'GET' && url === '/users' ) {
+        return res.end('Listagem de usuários')
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de usuário')
+    }
+
     return res.end('Hello World')
 })
 

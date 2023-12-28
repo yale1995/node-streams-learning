3914778154b140b046f48418328fcf03c978bbda
Yale Araujo <yale850@gmail.com>
2023-12-28 18:55:59 -0300

    feat: receiving request body

--- src/server.js
+++ src/server.js
@@ -23,32 +23,46 @@ import http from 'node:http'
 
 // 1. Content-type --> Especificar tipo de conteúdo (JSON / String)
 // 2. Status-code --> Reportar o status da requisição (Success / Error) E o motivo do erro (Erro inesperado / Não autenticado, Credenciais Inválidas)
-    // 100 - 199 --> Informational responses
-    // 200 - 299 --> Successful responses
-    // 300 - 399 --> Redirectional messages
-    // 400 - 499 --> Client error responses
-    // 500 - 599 --> Server error responses
+// 100 - 199 --> Informational responses
+// 200 - 299 --> Successful responses
+// 300 - 399 --> Redirectional messages
+// 400 - 499 --> Client error responses
+// 500 - 599 --> Server error responses
 
 
 const users = []
 
 
-const server = http.createServer((req, res) => {
-    const {method, url} = req
-    
-    if (method === 'GET' && url === '/users' ) {
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
+
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = null
+    }   
+
+    if (method === 'GET' && url === '/users') {
 
         return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+            .setHeader('Content-type', 'application/json')
+            .end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users') {
-
+        const { name, email } = req.body
         users.push({
             id: 1,
-            name: 'John Doe',
-            email: 'johndoe@example.com'
+            name,
+            email
         })
 
 
